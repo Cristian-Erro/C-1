9ff54386202f2f2188e966326e724b293d67119d
Cristian Erro <cristianlp86@gmail.com>
2022-10-10 18:59:46 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Programacion_ej_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("******Buscar taza*****");
+            Console.ReadKey();
+            Console.WriteLine("Buscar saquito de cafe");
+            Console.ReadKey();
+            Console.WriteLine("*****Calentar agua****");
+            Console.ReadKey();
+            Console.WriteLine("*Poner saquito en taza");
+            Console.ReadKey();
+            Console.WriteLine("****Agregar agua******");
+            Console.ReadKey();
+        }
+    }
+}
